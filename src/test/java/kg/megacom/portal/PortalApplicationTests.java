c587745dde60785363b636efd03f2fc404854099
smadankulov <smadankulov@megacom.kg>
2025-07-07 14:48:42 +0600

    #1test / test for getAll employees

--- src/test/java/kg/megacom/portal/PortalApplicationTests.java
+++ src/test/java/kg/megacom/portal/PortalApplicationTests.java
@@ -1,13 +1,35 @@
 package kg.megacom.portal;
 
+import kg.megacom.portal.controllers.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class PortalApplicationTests {
+
+    @Autowired
+    private AuthController authController;
+    @Autowired
+    private EmployeeController employeeController;
+    @Autowired
+    private KnowledgeBaseController knowledgeBaseController;
+    @Autowired
+    private NewsController newsController;
+    @Autowired
+    private RepairRequestController repairRequestController;
 
     @Test
     void contextLoads() {
+        assertNotNull(authController);
+        assertNotNull(employeeController);
+        assertNotNull(knowledgeBaseController);
+        assertNotNull(newsController);
+        assertNotNull(repairRequestController);
     }
 
 }
---
+++ src/test/java/kg/megacom/portal/contollers/EmployeeControllerTest.java
@@ -0,0 +1,81 @@
+package kg.megacom.portal.contollers;
+
+import kg.megacom.portal.controllers.EmployeeController;
+import kg.megacom.portal.models.dto.DepartmentDTO;
+import kg.megacom.portal.models.dto.EmployeeDTO;
+import kg.megacom.portal.repositories.EmployeeRepository;
+import kg.megacom.portal.services.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EmployeeController.class)
+@ActiveProfiles("dev")
+public class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private EmployeeService employeeService;
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    private final String BASE_URL = "/api/v1/employees";
+
+    @Test
+    void getEmployees_ShouldReturnOk() throws Exception {
+        Integer pageNumber = 0;
+        Integer limit = 10;
+
+        EmployeeDTO emp1 = EmployeeDTO.builder()
+                .email("email@email.com")
+                .companyNumber("1212")
+                .departmentName(DepartmentDTO.builder().name("DIT").build())
+                .firstName("firstName")
+                .lastName("lastName")
+                .fullName("fullName")
+                .identificator_1C("identificator_1C")
+                .mobileNumber("213123123")
+                .position("position")
+                .workOffice("workOffice")
+                .workPlace("workPlace")
+                .build();
+
+        EmployeeDTO emp2 = EmployeeDTO.builder()
+                .email("email@email1.com")
+                .companyNumber("1212")
+                .departmentName(DepartmentDTO.builder().name("COM").build())
+                .firstName("firstName")
+                .lastName("lastName")
+                .fullName("fullName")
+                .identificator_1C("identificator_1C")
+                .mobileNumber("213123123")
+                .position("position")
+                .workOffice("workOffice")
+                .workPlace("workPlace")
+                .build();
+
+        List<EmployeeDTO> employeesList = new ArrayList<>();
+        employeesList.add(emp1);
+        employeesList.add(emp2);
+
+        when(employeeService.findAll(eq(pageNumber), eq(limit))).thenReturn(employeesList);
+
+        mockMvc.perform(get(BASE_URL)
+                .param("pageNumber", pageNumber.toString())
+                .param("limit", limit.toString()))
+                .andExpect(status().isOk());
+    }
+}
